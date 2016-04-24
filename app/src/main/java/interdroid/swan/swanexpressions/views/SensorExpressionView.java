2c0366e083e34fa49d63e5ec78005a5faa5d7686
Steven <sluijts@gmail.com>
2016-04-24 14:46:47 +0200

    Improved sensor view

--- app/src/main/java/interdroid/swan/swanexpressions/views/SensorExpressionView.java
+++ app/src/main/java/interdroid/swan/swanexpressions/views/SensorExpressionView.java
@@ -1,21 +1,27 @@
 package interdroid.swan.swanexpressions.views;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.List;
+
 import interdroid.swan.swanexpressions.R;
+import interdroid.swan.swanexpressions.pojos.ConfigurationItem;
 import interdroid.swan.swanexpressions.pojos.expressions.ExpressionCreatorItem;
+import interdroid.swan.swanexpressions.pojos.expressions.SensorExpression;
 
 /**
  * Created by steven on 19/01/16.
  */
-public class SensorExpressionView extends CardView implements View.OnClickListener {
+public class SensorExpressionView extends FrameLayout implements View.OnClickListener {
 
-    private ImageView mSensorImage;
+    private TextView mSensorTitle;
+    private TextView mValuePath;
+    private LinearLayout mConfigurationContainer;
     private TextView mSensorExpression;
 
     private ExpressionCreatorItem mExpressionCreatorItem;
@@ -40,7 +46,9 @@ public class SensorExpressionView extends CardView implements View.OnClickListen
     }
 
     private void getViews() {
-        mSensorImage = (ImageView) findViewById(R.id.sensor_image);
+        mSensorTitle = (TextView) findViewById(R.id.sensor_title);
+        mValuePath = (TextView) findViewById(R.id.sensor_value_path);
+        mConfigurationContainer = (LinearLayout) findViewById(R.id.sensor_configuration_container);
         mSensorExpression = (TextView) findViewById(R.id.sensor_expression);
     }
 
@@ -48,6 +56,29 @@ public class SensorExpressionView extends CardView implements View.OnClickListen
                                          OnSensorExpressionClickListener listener) {
         mExpressionCreatorItem = expressionCreatorItem;
         mListener = listener;
+        SensorExpression sensorExpression = (SensorExpression) expressionCreatorItem.expressionInterface;
+        mSensorTitle.setText(sensorExpression.getSensor());
+        mValuePath.setText(sensorExpression.getValuePath());
+        List<ConfigurationItem> configurtionItems = sensorExpression.getConfigurationList();
+        mConfigurationContainer.removeAllViews();
+        for (int i = 0; i < configurtionItems.size(); i++) {
+            ConfigurationItem configurationItem = configurtionItems.get(i);
+            if (!configurationItem.key.contains("configuration_full")) {
+                TextView textView = new TextView(mConfigurationContainer.getContext());
+                textView.setText(configurationItem.key + ": " + configurationItem.value);
+                mConfigurationContainer.addView(textView);
+            } else {
+                String[] configurationFullParts = configurationItem.value.split("name");
+                String configurationName = configurationFullParts[1].replace("\":\"", "");
+                configurationName = configurationName.substring(0, configurationName.indexOf('"'));
+                String valueName = configurationFullParts[configurationFullParts.length - 1].replace("\":\"", "");
+                valueName = valueName.substring(0, valueName.indexOf('"'));
+                TextView textView = new TextView(mConfigurationContainer.getContext());
+                textView.setText(configurationName + ", " + valueName);
+                mConfigurationContainer.addView(textView);
+            }
+
+        }
         mSensorExpression.setText(expressionCreatorItem.expressionInterface.getExpression());
     }
 
